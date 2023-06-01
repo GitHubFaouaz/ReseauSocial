413d0fb249304b3c4ec5a61739a09bb5a1eced84
faouaz abdallah <faouazae@yahoo.com>
2023-06-01 09:31:51 +0200

    posts profilLeft

--- client/src/component/ProfileLeft/ProfileLeft.jsx
+++ client/src/component/ProfileLeft/ProfileLeft.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import FollowersCard from "../FollowersCard/FollowersCard";
-// import LogoSearch from "../LogoSearch/LogoSearch";
 import ProfileCard from "../ProfileCard/ProfileCard";
 
 function ProfileLeft() {
