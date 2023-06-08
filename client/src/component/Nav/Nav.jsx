67a8fd3f51680720fcbc450608c313a777924645
faouaz abdallah <faouazae@yahoo.com>
2023-06-08 12:43:32 +0200

    nav

--- client/src/component/Nav/Nav.jsx
+++ client/src/component/Nav/Nav.jsx
@@ -1,22 +1,27 @@
 import { NavLink } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 const Nav = () => {
-  const list = document.querySelectorAll(".navigation li");
-  function activelink() {
-    list.forEach((item) => item.classList.remove("active")); // <li className="active"> on supprime pour un li pour ajouter sur un autre li
-    this.classList.add("active");
-  }
-  // La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
-  list.forEach((item) => item.addEventListener("mouseover", activelink)); // on appelle la function activelink au click avec item comme parametre
-  // La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
+  const { user } = useSelector((state) => state.authReducer.authData);
+  // const list = document.querySelectorAll(".navigation li");
+  // function activelink() {
+  //   list.forEach((item) => item.classList.remove("active")); // <li className="active"> on supprime pour un li pour ajouter sur un autre li
+  //   this.classList.add("active");
+  // }
+  // // La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
+  // list.forEach((item) => item.addEventListener("mouseover", activelink)); // on appelle la function activelink au click avec item comme parametre
+  // // La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
 
   return (
     <>
       <ul className="navigation">
-        <li className="active">
+        <li className={({ isActive }) => (isActive ? "active" : "icon")}>
           <div className="containeIconNav">
             <span className="icon">
-              <NavLink to="../home" activeClassName="active">
+              <NavLink
+                to="../home" // className={({ isActive, isPending }) =>
+                //   isPending ? "pending" : isActive ? "active" : ""
+                // }
+              >
                 <ion-icon name="home-outline"></ion-icon>
               </NavLink>
             </span>
@@ -24,10 +29,13 @@ const Nav = () => {
           </div>
         </li>
 
-        <li>
+        <li className={({ isActive }) => (isActive ? "active" : "icon")}>
           <div className="containeIconNav">
             <span className="icon">
-              <NavLink to="../profile/id" activeClassName="active">
+              <NavLink
+                to={`/profile/${user._id}`}
+                // className={({ isActive }) => (isActive ? "active_link" : "")}
+              >
                 <ion-icon name="person-outline"></ion-icon>
               </NavLink>
             </span>
@@ -35,10 +43,13 @@ const Nav = () => {
             <span className="text">Profile</span>
           </div>
         </li>
-        <li>
+        <li className={({ isActive }) => (isActive ? "active" : "icon")}>
           <div className="containeIconNav">
             <span className="icon">
-              <NavLink to="" activeClassName="active">
+              <NavLink
+                to=""
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
                 <ion-icon name="camera-outline"></ion-icon>
               </NavLink>
             </span>
@@ -46,10 +57,13 @@ const Nav = () => {
           </div>
         </li>
 
-        <li>
+        <li className={({ isActive }) => (isActive ? "active" : "icon")}>
           <div className="containeIconNav">
             <span className="icon">
-              <NavLink to="" activeClassName="active">
+              <NavLink
+                to=""
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
                 <ion-icon name="settings-outline"></ion-icon>
               </NavLink>
             </span>
--- server/routers/UserRoute.js
+++ server/routers/UserRoute.js
@@ -12,10 +12,17 @@ import authMiddleWare from "../Middleware/auth.js";
 const router = express.Router();
 
 // user
+// router.get("/", getAllUsers); // accedé à tous les utilisateurs
+// router.get("/:id", getUser); // accedé à un seul utilisateur
+// router.put("/:id", authMiddleWare, updateUser); // modification d'un utilisateur
+// router.delete("/:id", authMiddleWare, deleteUser); // suppresion d'un utilisateur
+// router.put("/:id/follow", authMiddleWare, followUser);
+// router.put("/:id/unfollow", authMiddleWare, UnFollowUser);
+// export default router;
 router.get("/", getAllUsers); // accedé à tous les utilisateurs
 router.get("/:id", getUser); // accedé à un seul utilisateur
-router.put("/:id", authMiddleWare, updateUser); // modification d'un utilisateur
-router.delete("/:id", authMiddleWare, deleteUser); // suppresion d'un utilisateur
-router.put("/:id/follow", authMiddleWare, followUser);
-router.put("/:id/unfollow", authMiddleWare, UnFollowUser);
+router.put("/:id", updateUser); // modification d'un utilisateur
+router.delete("/:id", deleteUser); // suppresion d'un utilisateur
+router.put("/:id/follow", followUser);
+router.put("/:id/unfollow", UnFollowUser);
 export default router;
