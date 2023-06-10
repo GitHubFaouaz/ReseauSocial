0370f5701dd1658dd19c7b32ef698be00df08a4f
faouaz abdallah <faouazae@yahoo.com>
2023-06-10 13:43:34 +0200

    nav

--- client/src/component/Nav/Nav.jsx
+++ client/src/component/Nav/Nav.jsx
@@ -2,74 +2,52 @@ import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 const Nav = () => {
   const { user } = useSelector((state) => state.authReducer.authData);
-  // const list = document.querySelectorAll(".navigation li");
-  // function activelink() {
-  //   list.forEach((item) => item.classList.remove("active")); // <li className="active"> on supprime pour un li pour ajouter sur un autre li
-  //   this.classList.add("active");
-  // }
-  // // La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
-  // list.forEach((item) => item.addEventListener("mouseover", activelink)); // on appelle la function activelink au click avec item comme parametre
-  // // La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
 
   return (
     <>
       <ul className="navigation">
-        <li className={({ isActive }) => (isActive ? "active" : "icon")}>
-          <div className="containeIconNav">
+        <li>
+          <NavLink
+            to="../home"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
             <span className="icon">
-              <NavLink
-                to="../home" // className={({ isActive, isPending }) =>
-                //   isPending ? "pending" : isActive ? "active" : ""
-                // }
-              >
-                <ion-icon name="home-outline"></ion-icon>
-              </NavLink>
+              <ion-icon name="home-outline"></ion-icon>
             </span>
             <span className="text">Home</span>
-          </div>
+          </NavLink>
         </li>
 
-        <li className={({ isActive }) => (isActive ? "active" : "icon")}>
-          <div className="containeIconNav">
+        <li>
+          <NavLink
+            to={`/profile/${user._id}`}
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
             <span className="icon">
-              <NavLink
-                to={`/profile/${user._id}`}
-                // className={({ isActive }) => (isActive ? "active_link" : "")}
-              >
-                <ion-icon name="person-outline"></ion-icon>
-              </NavLink>
+              <ion-icon name="person-outline"></ion-icon>
             </span>
 
             <span className="text">Profile</span>
-          </div>
+          </NavLink>
         </li>
-        <li className={({ isActive }) => (isActive ? "active" : "icon")}>
-          <div className="containeIconNav">
-            <span className="icon">
-              <NavLink
-                to=""
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                <ion-icon name="camera-outline"></ion-icon>
-              </NavLink>
-            </span>
-            <span className="text">Messages</span>
-          </div>
+        {/*     <li>
+          <span className="icon">
+            <NavLink to="">
+              <ion-icon name="camera-outline"></ion-icon>
+            </NavLink>
+          </span>
+          <span className="text">Messages</span>
         </li>
 
-        <li className={({ isActive }) => (isActive ? "active" : "icon")}>
-          <div className="containeIconNav">
-            <span className="icon">
-              <NavLink
-                to=""
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                <ion-icon name="settings-outline"></ion-icon>
-              </NavLink>
-            </span>
-            <span className="text">Settings</span>
-          </div>
-        </li>
+        <li>
+          <span className="icon">
+      
+            <NavLink to="">
+              <ion-icon name="settings-outline"></ion-icon>
+            </NavLink>
+          </span>
+          <span className="text">Settings</span>
+        </li> */}
       </ul>
     </>
   );
