d879f50d164f137308af306a5d780b39500f575d
faouaz abdallah <faouaza@yahoo.com>
2023-07-31 00:45:15 +0200

    date

--- client/src/component/Post/Post.jsx
+++ client/src/component/Post/Post.jsx
@@ -28,7 +28,14 @@ const Post = ({ data }) => {
 
   // --------------------------------------------
   const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
-  
+  const date =new Date(updatedPost.updatedAt) 
+  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  const formattedDate = date.toLocaleString('fr-FR', options);//toLocaleString() est une méthode intégrée en JavaScript qui permet de formater les dates et les nombres en fonction des paramètres régionaux spécifiés
+  //dateObject.toLocaleString([locales[, options]])
+// dateObject : Un objet Date ou une valeur de temps numérique (timestamp) que vous souhaitez formater.
+// locales (facultatif) : Une chaîne de caractères qui indique la locale ou une liste de locales pour laquelle vous souhaitez obtenir une représentation locale. Par exemple, "fr-FR" pour le français de France.
+// options (facultatif) : Un objet contenant des options supplémentaires pour le formatage. Cela peut inclure des options pour personnaliser le style de la date (court, moyen, long), le format des nombres, etc.
+  console.log(formattedDate);
   useEffect(()=> {
     //  const updatedPost = posts.find((post) => post.userId === user._id ); 
      setUpdatedPost(posts.find((post) => post.userId === user._id )); 
@@ -69,7 +76,7 @@ const Post = ({ data }) => {
           {/* <span>
             <i>{likes}</i> likes
           </span> */}
-          {isUpdate? (<span>{updatedPost.desc}</span>) :(
+          {isUpdate? ( <div> <span>{updatedPost.desc}</span> <span>{formattedDate}</span></div>  )  :(
            <>
           <textarea 
           defaultValue={updatedPost.desc}
