3a87b45f7c6855c034e80b788e252ec782350829
faouaz abdallah <faouaza@yahoo.com>
2023-12-07 14:40:52 +0100

    comment

--- client/src/component/Post/CommentsPost.jsx
+++ client/src/component/Post/CommentsPost.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
-import ButtonSubmitComments from '../utils/BouttonSubmitComments/ButtonSubmitComments';
-import { useDispatch, useSelector } from 'react-redux';
-import FormattedData from '../utils/FormattedData/FormattedData';
-import { addCommentPost, getPost, getTimelinePosts } from '../../actions/PostsAction';
+import React, { useState } from "react";
+import ButtonSubmitComments from "../utils/BouttonSubmitComments/ButtonSubmitComments";
+import { useDispatch, useSelector } from "react-redux";
+import FormattedData from "../utils/FormattedData/FormattedData";
+import {
+  addCommentPost,
+  getPost,
+  getTimelinePosts,
+} from "../../actions/PostsAction";
 
 const CommentsPost = ({ post, user }) => {
   const [text, setText] = useState();
@@ -12,56 +16,54 @@ const CommentsPost = ({ post, user }) => {
 
   // const handleComments = async (e) => {
   const handleComments = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (text) {
-      dispatch(addCommentPost(post._id, user.id, text))
+      dispatch(addCommentPost(post._id, user.id, text));
       // dispatch(getPost(post._id))
+      dispatch(getTimelinePosts());
 
-      setText('')
+      setText("");
     }
-  }
+  };
   return (
-
     <div className="containe-postComment">
-      < div className="commentsLength" >
-
+      <div className="commentsLength">
         {post.comments.map((comment) => {
           // console.log("textCommentaire : ", comment.text)
           // console.log('comment', comment);
           return (
             <div className="containe-imgName" key={comment._id}>
-              <img src={
-                user.profilePicture
-                  ? serverPublic + user.profilePicture
-                  : serverPublic + "defaultProfile.png"
-              } alt="" />
+              <img
+                src={
+                  user.profilePicture
+                    ? serverPublic + user.profilePicture
+                    : serverPublic + "defaultProfile.png"
+                }
+                alt=""
+              />
               <div className="containe-nameAndText">
                 <span>{user.firstname}</span>
                 <span>{comment.text}</span>
               </div>
-              <p><FormattedData post={comment.timestamp} /></p>
-
-            </div >
-          )
-        })
-
-        }
-
-
-
-      </div >
-
-
-
-      <div className="containe-inputComments">
-        <input type='text' placeholder="Laissez un commentaire" onChange={(e) => setText(e.target.value)} value={text} />
-        <ButtonSubmitComments texte={'Envoyer'} onClick={handleComments} />
-
+              <p>
+                <FormattedData post={comment.timestamp} />
+              </p>
+            </div>
+          );
+        })}
       </div>
 
-    </div >
-
+      <div className="containe-inputComments">
+        <input
+          type="text"
+          placeholder="Laissez un commentaire"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <ButtonSubmitComments texte={"Envoyer"} onClick={handleComments} />
+      </div>
+    </div>
   );
 };
 
